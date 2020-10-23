dea6ea0a3a92762e5402f7256e8f450af1aad5e6
rishabhchawla99899 <42553783+rishabhchawla99899@users.noreply.github.com>
2020-10-23 15:39:04 +0530

    This is my contribution.
    This is my contribution.

---
+++ sol.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   vector < pair <int, int> > ret;
+   static bool cmp(pair <int, int>& a, pair <int, int>& b){
+      return a.first == b.first ? a.second < b.second : a.first < b.first;
+   }
+   int getTurn(int n){
+      int ret = 0;
+      while(n != 1){
+         if(n & 1){
+            n = n * 3 + 1;
+         }
+         else n >>= 1;
+            ret ++;
+      }
+      return ret;
+   }
+   int getKth(int lo, int hi, int k) {
+      for(int i = lo; i <= hi; i++){
+         pair <int, int> temp;
+         temp.first = getTurn(i);
+         temp.second = i;
+         ret.push_back(temp);
+      }
+      sort(ret.begin(), ret.end(), cmp);
+      return ret[k - 1].second;
+   }
+};
